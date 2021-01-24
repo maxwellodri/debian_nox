986e2c034746f67d75a3d26f2f1bdc3175343855
Maxwell Odri <maxwellodri@gmail.com>
2021-01-24 23:40:48 +1000

    added gh-cli

--- debian_startup.sh
+++ debian_startup.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
-sudo apt update && sudo apt upgrade -y && apt install git neovim zsh exa -y
+sudo apt update && sudo apt upgrade -y && apt install git neovim zsh exa nodejs ctags gnupg software-properties-common -y #vim plugin rely on ctags,nodejs
+
+#github-cli:
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+sudo apt-add-repository https://cli.github.com/packages
+sudo apt update 
+sudo apt install gh -y 
+#grab dotfiles
 mkdir source && cd source && git clone https://github.com/maxwellodri/dotfiles && ./makesymlinks.sh nox && ./vim_plugins.sh
 mkdir ~/bin
+sh custom_bin_scripts.sh
 echo "In vim (neovim) run 'PlugInstall'"
 echo -e "Now run:\nchsh root --shell /bin/zsh"
